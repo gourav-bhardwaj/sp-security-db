5ad8dfd548c646f4e8d38756b1f994362a493398
gourav <gk97723747@gmail.com>
2021-07-02 22:55:20 +0530

    authentication issue done.

--- src/main/java/com/sp/SpSecurityConfig.java
+++ src/main/java/com/sp/SpSecurityConfig.java
@@ -1,17 +1,24 @@
 package com.sp;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.sp.constants.Roles;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
